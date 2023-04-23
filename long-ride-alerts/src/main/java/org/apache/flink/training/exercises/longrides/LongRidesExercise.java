81c30af38c0dee2781cfb7bdfb646879be5afc58
Huston <hustonic@gmail.com>
2023-04-24 00:28:53 +0800

    State变量增加transient关键字

--- long-ride-alerts/src/main/java/org/apache/flink/training/exercises/longrides/LongRidesExercise.java
+++ long-ride-alerts/src/main/java/org/apache/flink/training/exercises/longrides/LongRidesExercise.java
@@ -103,7 +103,7 @@ public class LongRidesExercise {
     @VisibleForTesting
     public static class AlertFunction extends KeyedProcessFunction<Long, TaxiRide, Long> {
 
-        private ValueState<TaxiRide> lastEvent;
+        private transient ValueState<TaxiRide> lastEvent;
 
         @Override
         public void open(Configuration config) throws Exception {
